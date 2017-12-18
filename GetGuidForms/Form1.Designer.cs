1350a524aed98b55f6235e54fca46893216effea
Yashar <yagharab@microsoft.com>
2017-12-18 15:13:08 -0800

    Adding support for upper/lowercase Guid generation

--- GetGuidForms/Form1.Designer.cs
+++ GetGuidForms/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.UpperCaseCheckBox = new System.Windows.Forms.CheckBox();
             this.GetGuidMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -94,11 +95,24 @@
             this.textBox1.Size = new System.Drawing.Size(275, 163);
             this.textBox1.TabIndex = 3;
             // 
+            // UpperCaseCheckBox
+            // 
+            this.UpperCaseCheckBox.AutoSize = true;
+            this.UpperCaseCheckBox.Checked = true;
+            this.UpperCaseCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.UpperCaseCheckBox.Location = new System.Drawing.Point(85, 70);
+            this.UpperCaseCheckBox.Name = "UpperCaseCheckBox";
+            this.UpperCaseCheckBox.Size = new System.Drawing.Size(103, 17);
+            this.UpperCaseCheckBox.TabIndex = 4;
+            this.UpperCaseCheckBox.Text = "Uppercase Guid";
+            this.UpperCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.UpperCaseCheckBox);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -121,6 +135,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox UpperCaseCheckBox;
     }
 }
 
